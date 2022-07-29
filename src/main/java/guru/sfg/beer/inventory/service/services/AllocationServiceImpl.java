259874babe2dab7c8e91770981dfe20e4d85ec6f
Oleksii Kostetskyi <okostet@softserveinc.com>
2022-07-29 16:51:22 +0300

    Fix method, add readability

--- src/main/java/guru/sfg/beer/inventory/service/services/AllocationServiceImpl.java
+++ src/main/java/guru/sfg/beer/inventory/service/services/AllocationServiceImpl.java
@@ -7,6 +7,7 @@ import guru.sfg.brewery.model.BeerOrderLineDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -21,6 +22,7 @@ public class AllocationServiceImpl implements AllocationService {
 
     private final BeerInventoryRepository beerInventoryRepository;
 
+    @Transactional
     @Override
     public Boolean allocateOrder(BeerOrderDto beerOrderDto) {
         log.debug("Allocating OrderId: " + beerOrderDto.getId());
@@ -29,12 +31,15 @@ public class AllocationServiceImpl implements AllocationService {
         AtomicInteger totalAllocated = new AtomicInteger();
 
         beerOrderDto.getBeerOrderLines().forEach(beerOrderLine -> {
-            if ((((beerOrderLine.getOrderQuantity() != null ? beerOrderLine.getOrderQuantity() : 0)
-                    - (beerOrderLine.getQuantityAllocated() != null ? beerOrderLine.getQuantityAllocated() : 0)) > 0)) {
+            int orderedQty = beerOrderLine.getOrderQuantity() != null ? beerOrderLine.getOrderQuantity() : 0;
+            int previouslyAllocatedQty = beerOrderLine.getQuantityAllocated() != null ? beerOrderLine.getQuantityAllocated() : 0;
+            if ((orderedQty - previouslyAllocatedQty) > 0) {
+                beerOrderLine.setOrderQuantity(orderedQty);
+                beerOrderLine.setQuantityAllocated(previouslyAllocatedQty);
                 allocateBeerOrderLine(beerOrderLine);
             }
-            totalOrdered.set(totalOrdered.get() + beerOrderLine.getOrderQuantity());
-            totalAllocated.set(totalAllocated.get() + (beerOrderLine.getQuantityAllocated() != null ? beerOrderLine.getQuantityAllocated() : 0));
+            totalOrdered.addAndGet(orderedQty);
+            totalAllocated.addAndGet(beerOrderLine.getQuantityAllocated());
         });
 
         log.debug("Total Ordered: " + totalOrdered.get() + " Total Allocated: " + totalAllocated.get());
@@ -47,10 +52,9 @@ public class AllocationServiceImpl implements AllocationService {
 
         beerInventoryList.forEach(beerInventory -> {
             int inventory = (beerInventory.getQuantityOnHand() == null) ? 0 : beerInventory.getQuantityOnHand();
-            int orderQty = (beerOrderLine.getOrderQuantity() == null) ? 0 : beerOrderLine.getOrderQuantity();
-            int allocatedQty = (beerOrderLine.getQuantityAllocated() == null) ? 0 : beerOrderLine.getQuantityAllocated();
+            int orderQty = beerOrderLine.getOrderQuantity();
+            int allocatedQty = beerOrderLine.getQuantityAllocated();
             int qtyToAllocate = orderQty - allocatedQty;
-
             if (inventory >= qtyToAllocate) { // full allocation
                 inventory = inventory - qtyToAllocate;
                 beerOrderLine.setQuantityAllocated(orderQty);
